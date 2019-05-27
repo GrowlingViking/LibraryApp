454898c8b2f230b17d3ccf20e6a88fc21b89d16c
Sigurd Gravning <sigurd@unimicro.no>
2019-05-27 14:37:57 +0200

    Add models and context

---
+++ LibraryApi/Models/Book.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryApi.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int NrOfCopies { get; set; }
+    }
+}
---
+++ LibraryApi/Models/LibraryContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}

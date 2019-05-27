30cc72a6fa0a9abf049adaa0504db3200f10b128
Sigurd Gravning <sigurd@unimicro.no>
2019-05-27 14:45:51 +0200

    Add author property to book

--- LibraryApi/Models/Book.cs
+++ LibraryApi/Models/Book.cs
@@ -12,5 +12,6 @@ namespace LibraryApi.Models
         public string Description { get; set; }
         public string Genre { get; set; }
         public int NrOfCopies { get; set; }
+        public string Author { get; set; }
     }
 }
